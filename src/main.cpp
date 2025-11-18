351e4a39eb9f43de17c1ff2b982378d1ae4f5c96
AnnoyingRains <annoyingrain5@gmail.com>
2025-11-19 01:15:53 +1100

    feat(main): Use fastLED to toggle an example LED strip

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,42 @@
 #include <Arduino.h>
+#include <FastLED.h>
 #include <WiFi.h>
 #include <esp_now.h>
 #include <libTailSync.h>
 
+#define LED_COUNT 10
+
 uint8_t Broadcast_MAC[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
+CRGB leds[LED_COUNT] = {};
+
 void handleColourPacket(ColourPacket packet) {
-  Serial.printf("[LOG]: colour! r: %d g: %d b: %d", packet.colour[0][0].red,
-                packet.colour[0][0].green, packet.colour[0][0].blue);
-  Serial.printf("[LOG]: Average head colour: %d",
-                AverageColour(packet.colour[1][3], packet.colour[1][4],
-                              packet.colour[2][3], packet.colour[2][4]));
+  for (int i = 0; i < LED_COUNT; i++) {
+    uint16_t row = i / 8;
+    uint16_t col = i % 8;
+    leds[i] = CRGB{packet.colour[row][col].red, packet.colour[row][col].green,
+                   packet.colour[row][col].blue};
+  }
+  FastLED.show();
 }
 
-void handlePulsePacket() { Serial.println("[LOG]: pulse!"); }
+void handlePulsePacket() { Serial.println("[LOG]: pulse!\r\n"); }
 
-void handleEndSessionPacket() { Serial.println("[LOG]: End session"); }
+void handleEndSessionPacket() { Serial.println("[LOG]: End session\r\n"); }
+void handleMetachange(MetaPacket packet) {
+  Serial.printf("LOG: Meta changed! Channel Name: %s DJ Name: %s, Song Name: "
+                "%s, Colour Rate: %d\r\n",
+                packet.channelName, packet.djName, packet.songName,
+                packet.colourRate);
+}
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(921600);
   WiFi.mode(WIFI_STA);
   // Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
     while (1) {
-      Serial.println("[FATAL]: Failed to initialize ESP-NOW");
+      Serial.println("[FATAL]: Failed to initialize ESP-NOW\r\n");
     }
   }
 
@@ -33,7 +46,7 @@ void setup() {
   peerInfo.encrypt = false;
   if (esp_now_add_peer(&peerInfo) != ESP_OK) {
     while (1) {
-      Serial.println("[FATAL]: Failed to initialize ESP-NOW peer");
+      Serial.println("[FATAL]: Failed to initialize ESP-NOW peer\r\n");
     }
   }
 
@@ -42,6 +55,12 @@ void setup() {
   setColourCallback(handleColourPacket);
   setPulseCallback(handlePulsePacket);
   setEndSessionCallback(handleEndSessionPacket);
+  setMetaChangeCallback(handleMetachange);
+
+  FastLED.addLeds<WS2812, 1, GRB>(leds, LED_COUNT)
+      .setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(255);
+  FastLED.show(); // turn the LEDs off until we get data
 }
 
 void loop() {
